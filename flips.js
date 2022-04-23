25258eff14f147792cd42063df2543cdb335ca9a
KevinLan7 <kaiwenl@ad.unc.edu>
2022-04-23 16:25:07 -0400

    testAgain1

--- flips.js
+++ flips.js
@@ -1,11 +1,13 @@
+let startNum = 2;
+let defaultFlip = 1;
 import {Require1} from "model";
 let require = Require1(import.meta.url);
 let minimist = require("minimist");
 let {exit} = require("process");
 import {coinFlips,countFlips} from "./modules/coin.mjs";
-let allArg = minimist(process.argv.slice(2));
+let allArg = minimist(process.argv.slice(startNum));
 let argFlips = allArg["number"];
-let numFlips = argFlips || 1;
+let numFlips = argFlips || defaultFlip;
 let resultCoinFlips = coinFlips(numFlips);
 console.log(resultCoinFlips);
 console.log(countFlips(resultCoinFlips));
--- guess-flip.js
+++ guess-flip.js
@@ -1,9 +1,10 @@
+let startNum = 2;
 import {Require1} from "model";
 let require = Require1(import.meta.url);
 let minimist = require("minimist");
 let {exit} = require("process");
 import {flipACoin} from "./modules/coin.mjs";
-let allArg = minimist(process.argv.slice(2));
+let allArg = minimist(process.argv.slice(startNum));
 let argCall = allArg["call"];
 try{
     let resultCoinFlips1 = flipACoin(argCall);
